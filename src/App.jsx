debd20dfaded230846132c890e34cbdd831b1ae4
Pray Suthar <praysuthar513@gmail.com>
2023-10-20 13:24:38 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <h1>Quote Generator</h1>
+      <div className='container'>
+        <p></p>
+      </div>
+    </>
+  )
+}
+
+export default App
